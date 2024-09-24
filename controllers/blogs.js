ac639dd0aa6d1a7cba029c012f7ef298f8309488
Geneticanomaly <thomaskaatranen@gmail.com>
2024-09-24 22:53:14 +0300

    Addition of task 13.6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,11 @@
 const blogsRouter = require('express').Router();
 const { Blog } = require('../models');
 
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id);
+    next();
+};
+
 blogsRouter.get('/', async (req, res) => {
     const blogs = await Blog.findAll();
     res.json(blogs);
@@ -15,11 +20,19 @@ blogsRouter.post('/', async (req, res) => {
     }
 });
 
-blogsRouter.delete('/:id', async (req, res) => {
-    const blog = await Blog.findByPk(req.params.id);
-
-    if (blog) await blog.destroy();
+blogsRouter.delete('/:id', blogFinder, async (req, res) => {
+    if (req.blog) await req.blog.destroy();
     res.status(204).end();
+});
+
+blogsRouter.put('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+        req.blog.likes = req.body.likes;
+        await req.blog.save();
+        res.json(req.blog);
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = blogsRouter;
